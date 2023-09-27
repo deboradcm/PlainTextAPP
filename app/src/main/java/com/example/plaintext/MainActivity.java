991581ab607532211f2df3077f6ba2216f90cc12
Débora Medeiros <debora.medeiros@icomp.ufam.edu.br>
2023-09-27 13:16:38 -0400

    Primeiras telas

---
+++ app/src/main/java/com/example/plaintext/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.plaintext;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+    }
